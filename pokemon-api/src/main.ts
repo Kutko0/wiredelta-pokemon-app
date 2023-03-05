88c1fa568fcd10976d0c485639c1ab8fcd8af1d2
Andrej Kutliak <andrej.kutliak@behavee.com>
2023-03-05 15:30:49 +0100

    Stashed for laptop work

--- pokemon-api/src/main.ts
+++ pokemon-api/src/main.ts
@@ -4,11 +4,24 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const newUser = prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    password: 'strongPass',
+  },
+});
+newUser.then((data) => {
+  console.log(data);
+});
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
   await app.listen(3000);
 }
